d9eb09a5c6d0b708755bdbef131285291f518c45
koziejka <koziejka.com@gmail.com>
2017-06-08 21:09:09 +0200

    remaster

--- scripts.js
+++ scripts.js
@@ -1,25 +1,63 @@
 const keycode = document.getElementById('keycode')
 const keyinfo = document.getElementById('key')
 const history = document.getElementById('history')
+const Case = document.getElementById('case')
 
 history.createBindList(x => {
     x = x.split('|')
-    const wraper = Node.create("div", { class: "wraper" })
-    const key = Node.create("span", { class: "key" }, x[1])
-    const code = Node.create("span", { class: "code" }, x[0])
+    const wraper = Node.create('div', { class: 'wraper' })
+    const key = Node.create('span', { class: 'key' }, x[1])
+    const code = Node.create('span', { class: 'code' }, x[0])
     wraper.add([code, key])
     return wraper
 })
 
 window.addEventListener('keydown', e => {
-    keycode.innerHTML = e.keyCode // code, key
-    keyinfo.innerHTML = e.code.replace("Key", "").replace("Digit", "")
     e.preventDefault()
-    const code = `${e.keyCode}|${e.code.replace("Key", "").replace("Digit", "")}`
+    keycode.innerHTML = e.keyCode // code, key
+    
+    let keyname = e.code
+        .replace('Key', '')
+        .replace('Digit', '')
+    if (/([A-Z]\w+)([A-Z]\w+)/.test(keyname)) {
+        const temp = keyname.match(/([A-Z]\w+)([A-Z]\w+)/)
+        keyname = `${temp[2]} ${temp[1]}`
+    } else if (/(\w+)(\d+)/.test(keyname)) {
+        const temp = keyname.match(/(\w+)(\d+)/)
+        keyname = `${temp[1]} ${temp[2]}`
+    }
+    
+    keyinfo.innerHTML = keyname
+    const code = `${e.keyCode}|${keyname}`
     let list = history.bindList
     let index = list.indexOf(code)
     if (index != -1) list.splice(index, 1)
     list.unshift(code)
     history.bindList = list
+
+    let string = 'switch (e.keyCode) {\n'
+    for (let s of list) {
+        s = s.split('|')
+        string += `    case ${s[0]}: // ${s[1]} \n        break\n`
+    }
+    string += '}'
+    Case.value = string
 })
 
+function copyToClipboard() {
+    Case.focus()
+    Case.setSelectionRange(0, Case.value.length)
+
+    try {
+        var succeed = document.execCommand('copy')
+    } catch (e) {
+        var succeed = false
+    }
+
+    return succeed
+}
+
+document.getElementById('copy').addEventListener('click', () => {
+    copyToClipboard()
+    alert('copied to clipboard')
+})
